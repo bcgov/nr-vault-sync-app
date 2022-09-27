b2fd13acc779737b0bf60f315b1fdabd7ea8e6cc
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2022-09-27 09:36:43 -0700

    feat: System template for Windows fluent bit deployment

--- src/vault/policy-roots/impl/system-policy.service.ts
+++ src/vault/policy-roots/impl/system-policy.service.ts
@@ -47,6 +47,7 @@ export class SystemPolicyService implements PolicyRootService<undefined> {
       {group: VAULT_ROOT_SYSTEM, templateName: 'db-admin-super', data: {secertDbPath: 'db'}},
       {group: VAULT_ROOT_SYSTEM, templateName: 'isss-cdua-read'},
       {group: VAULT_ROOT_SYSTEM, templateName: 'isss-ci-read'},
+      {group: VAULT_ROOT_SYSTEM, templateName: 'oraapp-imborapp-read'},
       {group: VAULT_ROOT_SYSTEM, templateName: 'user-generic'},
       {group: VAULT_ROOT_SYSTEM, templateName: 'vault-sync'},
     ];
