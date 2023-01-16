28f82d0a5e24bd4c7d78583d742717fc22d38ea9
Nisarg Vadgama <nisarg.vadgama@gov.bc.ca>
2023-01-16 11:56:58 -0800

    Add json log

--- .github/workflows/vault-token-script.sh
+++ .github/workflows/vault-token-script.sh
@@ -11,6 +11,7 @@ TEMP_FILE=$(mktemp)
 cat ./vault-config-intention.json | jq ".event.url=\"$GITHUB_SERVER_URL$GITHUB_ACTION_PATH" | \
             .user.id=\"$GITHUB_ACTOR@github" \
         " >> $TEMP_FILE
+cat $TEMP_FILE
 RESPONSE=$(curl -s -X POST $BROKER_URL/v1/intention/open \
     -H 'Content-Type: application/json' \
     -H "Authorization: Bearer $BROKER_JWT" \
