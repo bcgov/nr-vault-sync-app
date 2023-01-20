345ffd777b3b250678e5162e771d9b9cb151e7bb
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-01-20 12:14:21 -0800

    feat: broker workflow

--- .github/workflows/token-cleanup-script.sh
+++ .github/workflows/token-cleanup-script.sh
@@ -2,39 +2,6 @@
 
 cd "${0%/*}"
 
-INSTALL_VERSION="12.0.3"
-
-echo "===> Intention open"
-# Open intention
-# -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
-TEMP_FILE=$(mktemp)
-cat ./vault-config-intention.json | jq ".event.url=\"$GITHUB_SERVER_URL$GITHUB_ACTION_PATH\" | \
-            .user.id=\"$GITHUB_ACTOR@github\" \
-        " >> $TEMP_FILE
-RESPONSE=$(curl -s -X POST $BROKER_URL/v1/intention/open \
-    -H 'Content-Type: application/json' \
-    -H "Authorization: Bearer $BROKER_JWT" \
-    -d @$TEMP_FILE \
-    )
-echo "$BROKER_URL/v1/intention/open:"
-if [ "$(echo $RESPONSE | jq '.error')" != "null" ]; then
-    echo "Exit: Error detected"
-    exit 0
-fi
-
-# Save intention token for later
-INTENTION_TOKEN=$(echo $RESPONSE | jq -r '.token')
-echo "===> DB provision"
-
-# Get token for provisioning a db access
-DB_INTENTION_TOKEN=$(echo $RESPONSE | jq -r '.actions.database.token')
-
-# Get wrapped id for db access
-VAULT_TOKEN_WRAP=$(curl -s -X POST $BROKER_URL/v1/provision/token/self -H 'X-Broker-Token: '"$DB_INTENTION_TOKEN"'' -H 'X-Vault-Role-Id: '"$PROVISION_ROLE_ID"'')
-WRAPPED_VAULT_TOKEN=$(echo $VAULT_TOKEN_WRAP | jq -r '.wrap_info.token')
-
-UNWRAPPED_VAULT_TOKEN=$(curl -s -X POST $VAULT_ADDR/v1/sys/wrapping/unwrap -H 'X-Vault-Token: '"$WRAPPED_VAULT_TOKEN"'')
-
 VAULT_TOKEN=$(echo -n $UNWRAPPED_VAULT_TOKEN | jq -r '.auth.client_token')
 echo "===> Intention close"
 
--- .github/workflows/vault-token-script.sh
+++ .github/workflows/vault-token-script.sh
@@ -2,8 +2,6 @@
 
 cd "${0%/*}"
 
-INSTALL_VERSION="12.0.3"
-
 echo "===> Intention open"
 # Open intention
 # -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
@@ -12,12 +10,12 @@ cat ./vault-config-intention.json | jq ".event.url=\"$GITHUB_SERVER_URL$GITHUB_A
             .user.id=\"$GITHUB_ACTOR@github\" \
         " >> $TEMP_FILE
 cat $TEMP_FILE
-RESPONSE=$(curl -s -X POST $BROKER_URL/v1/intention/open \
+RESPONSE=$(curl -s -X POST $BROKER_ADDR/v1/intention/open \
     -H 'Content-Type: application/json' \
     -H "Authorization: Bearer $BROKER_JWT" \
     -d @$TEMP_FILE \
     )
-echo "$BROKER_URL/v1/intention/open:"
+echo "$BROKER_ADDR/v1/intention/open:"
 if [ "$(echo $RESPONSE | jq '.error')" != "null" ]; then
     echo "Exit: Error detected"
     exit 0
@@ -25,18 +23,17 @@ fi
 
 # Save intention token for later
 INTENTION_TOKEN=$(echo $RESPONSE | jq -r '.token')
-echo "INTENTION_TOKEN=$INTENTION_TOKEN" >> $ENV_INTENTION_TOKEN
+echo "INTENTION_TOKEN=$INTENTION_TOKEN" >> $GITHUB_ENV
 echo "===> DB provision"
 
 # Get token for provisioning a db access
-DB_INTENTION_TOKEN=$(echo $RESPONSE | jq -r '.actions.database.token')
+ACTION_TOKEN=$(echo $RESPONSE | jq -r '.actions.database.token')
 
 # Get wrapped id for db access
-VAULT_TOKEN_WRAP=$(curl -s -X POST $BROKER_URL/v1/provision/token/self -H 'X-Broker-Token: '"$DB_INTENTION_TOKEN"'' -H 'X-Vault-Role-Id: '"$PROVISION_ROLE_ID"'')
+VAULT_TOKEN_WRAP=$(curl -s -X POST $BROKER_ADDR/v1/provision/token/self -H 'X-Broker-Token: '"$ACTION_TOKEN"'' -H 'X-Vault-Role-Id: '"$PROVISION_ROLE_ID"'')
 WRAPPED_VAULT_TOKEN=$(echo $VAULT_TOKEN_WRAP | jq -r '.wrap_info.token')
 
 UNWRAPPED_VAULT_TOKEN=$(curl -s -X POST $VAULT_ADDR/v1/sys/wrapping/unwrap -H 'X-Vault-Token: '"$WRAPPED_VAULT_TOKEN"'')
 
 VAULT_TOKEN=$(echo -n $UNWRAPPED_VAULT_TOKEN | jq -r '.auth.client_token')
-echo "VAULT_TOKEN=$UNWRAPPED_VAULT_TOKEN" >> $ENV_VAULT_TOKEN
-echo "===> Intention close"
+echo "VAULT_TOKEN=$UNWRAPPED_VAULT_TOKEN" >> $GITHUB_ENV
