86f2945ea4e4350428b976c47f26ecc77a3765fc
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-01-20 13:45:01 -0800

    feat: add output

--- .github/workflows/vault-token-script.sh
+++ .github/workflows/vault-token-script.sh
@@ -3,6 +3,7 @@
 cd "${0%/*}"
 
 echo "===> Intention open"
+echo "$BROKER_ADDR/v1/intention/open:"
 # Open intention
 # -u "$BASIC_HTTP_USER:$BASIC_HTTP_PASSWORD" \
 TEMP_FILE=$(mktemp)
@@ -15,13 +16,13 @@ RESPONSE=$(curl -s -X POST $BROKER_ADDR/v1/intention/open \
     -H "Authorization: Bearer $BROKER_JWT" \
     -d @$TEMP_FILE \
     )
-echo "$BROKER_ADDR/v1/intention/open:"
 if [ "$(echo $RESPONSE | jq '.error')" != "null" ]; then
     echo "Exit: Error detected"
-    echo $RESPONSE
-    exit 0
+    echo $RESPONSE | jq '.'
+    exit -1
 fi
 
+echo "===> Intention save"
 # Save intention token for later
 INTENTION_TOKEN=$(echo $RESPONSE | jq -r '.token')
 echo "::add-mask::$INTENTION_TOKEN"
@@ -30,21 +31,26 @@ echo "INTENTION_TOKEN=$INTENTION_TOKEN" >> $GITHUB_ENV
 
 # Get token for provisioning a db access
 ACTION_TOKEN=$(echo $RESPONSE | jq -r '.actions.configure.token')
+echo "::add-mask::$ACTION_TOKEN"
 
+
+echo "===> Provision token"
 # Get wrapped id for db access
 WRAPPED_VAULT_TOKEN_JSON=$(curl -s -X POST $BROKER_ADDR/v1/provision/token/self -H 'X-Broker-Token: '"$ACTION_TOKEN"'' -H 'X-Vault-Role-Id: '"$PROVISION_ROLE_ID"'')
 if [ "$(echo $WRAPPED_VAULT_TOKEN_JSON | jq '.error')" != "null" ]; then
     echo "Exit: Error detected"
-    echo $WRAPPED_VAULT_TOKEN_JSON
-    exit 0
+    echo $WRAPPED_VAULT_TOKEN_JSON | jq '.'
+    exit -1
 fi
 WRAPPED_VAULT_TOKEN=$(echo $WRAPPED_VAULT_TOKEN_JSON | jq -r '.wrap_info.token')
+echo "::add-mask::$WRAPPED_VAULT_TOKEN"
 
+echo "===> Unwrap token"
 VAULT_TOKEN_JSON=$(curl -s -X POST $VAULT_ADDR/v1/sys/wrapping/unwrap -H 'X-Vault-Token: '"$WRAPPED_VAULT_TOKEN"'')
 if [ "$(echo $VAULT_TOKEN_JSON | jq '.error')" != "null" ]; then
     echo "Exit: Error detected"
-    echo $VAULT_TOKEN_JSON
-    exit 0
+    echo $VAULT_TOKEN_JSON | jq '.'
+    exit -1
 fi
 
 VAULT_TOKEN=$(echo -n $VAULT_TOKEN_JSON | jq -r '.auth.client_token')
