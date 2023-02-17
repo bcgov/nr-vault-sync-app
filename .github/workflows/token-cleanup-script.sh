10d3fc1366d6da39495871d5e5af7e96f877d4e0
Matthew Bystedt <mbystedt@gmail.com>
2023-02-17 08:32:01 -0800

    fix: vault token revoke (#22)

--- .github/workflows/token-cleanup-script.sh
+++ .github/workflows/token-cleanup-script.sh
@@ -5,7 +5,7 @@ cd "${0%/*}"
 echo "===> Revoke token"
 
 curl \
-    --header "X-Vault-Token: " \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
     --request POST \
     $VAULT_ADDR/v1/auth/token/revoke-self
 
