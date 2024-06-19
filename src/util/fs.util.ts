efa062606dd053c42fc741827c56b681468589a6
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-06-19 16:31:23 -0700

    refactor: use synchronous

--- src/util/fs.util.ts
+++ src/util/fs.util.ts
@@ -3,47 +3,31 @@ import { injectable } from 'inversify';
 
 @injectable()
 /**
- * Utility class for HCL
+ * Utility class for reading files
  */
 export default class FsUtil {
-  public readFile(filePath: string, encoding: BufferEncoding = 'utf-8'): void {
-    fs.lstat(filePath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
-      if (err) {
-        return console.error(`Error getting stats of the file: ${err.message}`);
-      }
-
+  public readFile(
+    filePath: string,
+    encoding: BufferEncoding = 'utf-8',
+  ): string | undefined {
+    try {
+      const stats = fs.lstatSync(filePath);
       if (stats.isSymbolicLink()) {
         // If the file is a symbolic link, resolve the actual path
-        fs.readlink(
-          filePath,
-          (err: NodeJS.ErrnoException | null, realPath: string) => {
-            if (err) {
-              return console.error(
-                `Error reading the symbolic link: ${err.message}`,
-              );
-            }
-
-            this.readActualFile(realPath, encoding);
-          },
-        );
+        const realPath = fs.readlinkSync(filePath);
+        return this.readActualFile(realPath, encoding);
       } else {
         // If it's not a symbolic link, read the file directly
-        this.readActualFile(filePath, encoding);
+        return this.readActualFile(filePath, encoding);
       }
-    });
+    } catch (err) {
+      console.error(
+        `Error getting stats of the file: ${(err as Error).message}`,
+      );
+    }
   }
 
-  private readActualFile(filePath: string, encoding: BufferEncoding): void {
-    fs.readFile(
-      filePath,
-      encoding,
-      (err: NodeJS.ErrnoException | null, data: string) => {
-        if (err) {
-          return console.error(`Error reading the file: ${err.message}`);
-        }
-
-        console.log(`File content:\n${data}`);
-      },
-    );
+  private readActualFile(filePath: string, encoding: BufferEncoding): string {
+    return fs.readFileSync(filePath, encoding);
   }
 }
