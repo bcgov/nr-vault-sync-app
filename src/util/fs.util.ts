680a29ae3f283a04ad16aaa95ab2e70db867a2ce
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-06-19 13:28:32 -0700

    feat: add explicit types

--- src/util/fs.util.ts
+++ src/util/fs.util.ts
@@ -1,23 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
 
-function readFile(filePath) {
-  fs.lstat(filePath, (err, stats) => {
+function readFile(filePath: string): void {
+  fs.lstat(filePath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
     if (err) {
       return console.error(`Error getting stats of the file: ${err.message}`);
     }
 
     if (stats.isSymbolicLink()) {
       // If the file is a symbolic link, resolve the actual path
-      fs.readlink(filePath, (err, realPath) => {
-        if (err) {
-          return console.error(
-            `Error reading the symbolic link: ${err.message}`,
-          );
-        }
+      fs.readlink(
+        filePath,
+        (err: NodeJS.ErrnoException | null, realPath: string) => {
+          if (err) {
+            return console.error(
+              `Error reading the symbolic link: ${err.message}`,
+            );
+          }
 
-        readActualFile(realPath);
-      });
+          readActualFile(realPath);
+        },
+      );
     } else {
       // If it's not a symbolic link, read the file directly
       readActualFile(filePath);
@@ -25,16 +27,20 @@ function readFile(filePath) {
   });
 }
 
-function readActualFile(filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      return console.error(`Error reading the file: ${err.message}`);
-    }
+function readActualFile(filePath: string): void {
+  fs.readFile(
+    filePath,
+    'utf-8',
+    (err: NodeJS.ErrnoException | null, data: string) => {
+      if (err) {
+        return console.error(`Error reading the file: ${err.message}`);
+      }
 
-    console.log(`File content:\n${data}`);
-  });
+      console.log(`File content:\n${data}`);
+    },
+  );
 }
 
 // Example usage
-const filePath = 'path/to/your/file/or/symlink.txt';
+const filePath: string = 'path/to/your/file/or/symlink.txt';
 readFile(filePath);
