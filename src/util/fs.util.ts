d646d6864784ac2c37d4e396be65f771eabe17fd
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-06-19 13:38:44 -0700

    refactor: convert to class

--- src/util/fs.util.ts
+++ src/util/fs.util.ts
@@ -1,42 +1,49 @@
 import * as fs from 'fs';
+import { injectable } from 'inversify';
 
-function readFile(filePath: string): void {
-  fs.lstat(filePath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
-    if (err) {
-      return console.error(`Error getting stats of the file: ${err.message}`);
-    }
-
-    if (stats.isSymbolicLink()) {
-      // If the file is a symbolic link, resolve the actual path
-      fs.readlink(
-        filePath,
-        (err: NodeJS.ErrnoException | null, realPath: string) => {
-          if (err) {
-            return console.error(
-              `Error reading the symbolic link: ${err.message}`,
-            );
-          }
-
-          readActualFile(realPath);
-        },
-      );
-    } else {
-      // If it's not a symbolic link, read the file directly
-      readActualFile(filePath);
-    }
-  });
-}
-
-function readActualFile(filePath: string): void {
-  fs.readFile(
-    filePath,
-    'utf-8',
-    (err: NodeJS.ErrnoException | null, data: string) => {
+@injectable()
+/**
+ * Utility class for HCL
+ */
+export default class FsUtil {
+  public readFile(filePath: string): void {
+    fs.lstat(filePath, (err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
       if (err) {
-        return console.error(`Error reading the file: ${err.message}`);
+        return console.error(`Error getting stats of the file: ${err.message}`);
       }
 
-      console.log(`File content:\n${data}`);
-    },
-  );
+      if (stats.isSymbolicLink()) {
+        // If the file is a symbolic link, resolve the actual path
+        fs.readlink(
+          filePath,
+          (err: NodeJS.ErrnoException | null, realPath: string) => {
+            if (err) {
+              return console.error(
+                `Error reading the symbolic link: ${err.message}`,
+              );
+            }
+
+            this.readActualFile(realPath);
+          },
+        );
+      } else {
+        // If it's not a symbolic link, read the file directly
+        this.readActualFile(filePath);
+      }
+    });
+  }
+
+  private readActualFile(filePath: string): void {
+    fs.readFile(
+      filePath,
+      'utf-8',
+      (err: NodeJS.ErrnoException | null, data: string) => {
+        if (err) {
+          return console.error(`Error reading the file: ${err.message}`);
+        }
+
+        console.log(`File content:\n${data}`);
+      },
+    );
+  }
 }
