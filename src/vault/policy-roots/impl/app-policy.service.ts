41664e4eb3da19469b97ffa19332f8cdb452f2e5
Matthew Bystedt <mbystedt@gmail.com>
2026-08-04 13:53:11 -0700

    feat: remove replaced cloud infrastructure pattern (#102)

--- src/vault/policy-roots/impl/app-policy.service.ts
+++ src/vault/policy-roots/impl/app-policy.service.ts
@@ -112,11 +112,6 @@ export class AppPolicyService implements PolicyRootService<Application> {
     });
     renderSpecs.push({
       group: VAULT_ROOT_APPS,
-      templateName: 'project-kv-infra',
-      data: policyData,
-    });
-    renderSpecs.push({
-      group: VAULT_ROOT_APPS,
       templateName: 'app-kv-read',
       data: policyData,
     });
